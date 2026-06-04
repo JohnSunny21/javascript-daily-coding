1c0a28a0dabd657d66d7fe6a328a8a2b89fbf917
JohnSunny <sunnyjohnballa@gmail.com>
2026-06-05 00:39:12 +0530

    feat: Adding the Jbelmud problem

--- freecodecamp/coding/JbelmudText.js
+++ freecodecamp/coding/JbelmudText.js
@@ -12,7 +12,12 @@ The input strings will contain no punctuation, and will be entirely lowercase.
 const { benchmark } = require("./utils/benchmark");
 
 
-
+const TESTCASES = [
+    [["hello world"], "hello wlord"],
+    [["i love jumbled text"], "i love jbelmud text"],
+    [["freecodecamp is my favorite place to learn to code"], "faccdeeemorp is my faiortve pacle to laern to cdoe"],
+    [["the quick brown fox jumps over the lazy dog"], "the qciuk borwn fox jmpus oevr the lazy dog"]
+];
 
 function jbelmu(text){
     const result = [];
@@ -32,6 +37,30 @@ function jbelmu(text){
     
     }
 
-    return result.join("");
-    
+    return result.join(" ");
+
 }
+
+function jbelmudText(str){
+    return str.split(" ").map(word => {
+        if(word.length <= 2){
+            // word of length 1 or 2 remain unchanged
+            return word;
+        }
+
+        const first = word[0];
+        const last = word[word.length - 1];
+        const middle = word.slice(1, -1).split("").sort().join("");
+        return first + middle + last;
+    }).join(" ");
+}
+
+
+if(require.main === module){
+    benchmark({"first": jbelmu, "second": jbelmudText}, TESTCASES, 10000);
+}
+
+
+
+
+module.exports = { jbelmu , jbelmudText};
